6687153e9aac76d51f2231f16cee8449a227e535
yish-mael <tolu.akinnubi@yahoo.com>
2020-12-23 18:55:18 +0100

    add logger to backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,16 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 
+app.use(morgan("dev"));
+
 app.get("/Home", (req, res) => {
     res.status(200).json({message: "Home page"});
+});
+
+app.post("/Home", (req, res) => {
+    res.status(200).json({message: "Home page post method"});
 });
 
 app.use((req, res) => {
